a59167df937a7724d4a6dd6bc38c3266109cb756
Piterden <efremov.a.denis@gmail.com>
2019-09-23 03:05:24 +0300

    Fixed eslint error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const JSCreateIncludesWebpackPlugin = require('./js-create-includes-webpack-plugin')
 
-
 module.exports = JSCreateIncludesWebpackPlugin
 
 module.exports = { JSCreateIncludesWebpackPlugin }
